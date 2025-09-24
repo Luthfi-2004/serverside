0902646d9bd0e84ef723d17b527ef7ee09a949b8
Luthfi-2004 <if23.luthfinaufal@mhs.ubpkarawang.ac.id>
2025-09-24 08:03:47 +0700

    oko

--- resources/views/aceline-gfn/index.blade.php
+++ resources/views/aceline-gfn/index.blade.php
@@ -8,11 +8,11 @@
       <div class="col-12">
 
         <div class="page-title-box d-flex align-items-center justify-content-between">
-          <h4 class="mb-0">GFN ACE LINE</h4>
+          <h4 class="mb-0">ACE LINE</h4>
           <div class="page-title-right">
             <ol class="breadcrumb m-0">
-              <li class="breadcrumb-item"><a href="javascript:void(0);">Greensand</a></li>
-              <li class="breadcrumb-item active">GFN ACE LINE</li>
+              <li class="breadcrumb-item"><a href="javascript:void(0);">ACE LINE</a></li>
+              <li class="breadcrumb-item active">GFN</li>
             </ol>
           </div>
         </div>
--- resources/views/jsh-gfn/index.blade.php
+++ resources/views/jsh-gfn/index.blade.php
@@ -8,11 +8,11 @@
                 <div class="col-12">
 
                     <div class="page-title-box d-flex align-items-center justify-content-between">
-                        <h4 class="mb-0">JSH GFN GREEN SAND</h4>
+                        <h4 class="mb-0">GFN</h4>
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript:void(0);">Greensand</a></li>
-                                <li class="breadcrumb-item active">Green Sand GFN</li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">JSH LINE</a></li>
+                                <li class="breadcrumb-item active">GFN</li>
                             </ol>
                         </div>
                     </div>
