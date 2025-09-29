e993fe96fe5f7055103e51e4fa2ffe39f9dbc14e
Luthfi-2004 <if23.luthfinaufal@mhs.ubpkarawang.ac.id>
2025-09-29 13:42:29 +0700

    dikit

--- resources/views/aceline-gfn/index.blade.php
+++ resources/views/aceline-gfn/index.blade.php
@@ -8,7 +8,7 @@
         <div class="col-12">
 
           <div class="page-title-box d-flex align-items-center justify-content-between">
-            <h4 class="mb-0">ACE LINE</h4>
+            <h4 class="mb-0">GFN</h4>
             <div class="page-title-right">
               <ol class="breadcrumb m-0">
                 <li class="breadcrumb-item"><a href="javascript:void(0);">ACE LINE</a></li>
